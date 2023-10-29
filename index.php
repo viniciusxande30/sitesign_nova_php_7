0576065cd4107a09058f238c02a8256a4a67bfd1
viniciusxande30 <vinicius.xande30@gmail.com>
2023-10-29 11:49:49 -0300

    Update

--- index.php
+++ index.php
@@ -1,26 +1,9 @@
 <?php
 require_once 'config.php';
 
-
-function obterUltimaParteDaURL() {
-    // Obtém o caminho (URI) da requisição
-    $caminho = $_SERVER['REQUEST_URI'];
-
-    // Usa basename para obter a última parte após a última barra
-    $ultimaParte = basename($caminho);
-
-    return $ultimaParte;
-}
-
-// Exemplo de uso
-$ultimaParteDaURL = obterUltimaParteDaURL();
-
-// Exibe a última parte da URL
-echo $ultimaParteDaURL;
-
 # "Sistema de rotas"...
 if (!isset($_GET['segment1'])) pages('index');
-if ($ultimaParteDaURL == 'auditoria') pages('auditoria');
+if ($_GET['segment1'] == 'auditoria') pages('auditoria');
 if ($_GET['segment1'] == 'bps') pages('bps');
 if ($_GET['segment1'] == 'clientes') pages('clientes');
 if ($_GET['segment1'] == 'consultoria-de-rh') pages('consultoria_de_rh');
